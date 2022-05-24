0e5c12915baafcc2494f4a3e0b4d84c7202e5f9a
Master <ehtiramabdullayev@gmail.com>
2022-05-25 00:22:07 +0200

    Update NoLinksReturnedException.java

--- src/main/java/com/scalable/capital/webcrawler/error/NoLinksReturnedException.java
+++ src/main/java/com/scalable/capital/webcrawler/error/NoLinksReturnedException.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.scalable.capital.webcrawler.error;
 
 /**
@@ -17,5 +12,4 @@ public class NoLinksReturnedException extends Exception{
     public NoLinksReturnedException(String string) {
         super(string);
     }
-    
 }
