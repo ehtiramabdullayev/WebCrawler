c81091d8eba8cfab0c519b22762fcaa0697a3b1c
Master <ehtiramabdullayev@gmail.com>
2022-05-25 00:21:30 +0200

    Update CheckSum.java

--- src/main/java/com/scalable/capital/webcrawler/core/CheckSum.java
+++ src/main/java/com/scalable/capital/webcrawler/core/CheckSum.java
@@ -1,22 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.scalable.capital.webcrawler.core;
 
 import static com.scalable.capital.webcrawler.util.GeneralUtils.getSHA1;
 import static com.scalable.capital.webcrawler.util.GeneralUtils.readBytesFromFile;
-
-
 
 /**
  *
  * @author Ehtiram Abdullayev
  */
 public class CheckSum {
-
-    
     public static String checkSumFile(String path){
         byte[] readBytesFromFile = readBytesFromFile(path);
         return getSHA1(readBytesFromFile);
@@ -24,5 +15,4 @@ public class CheckSum {
     public static String checkSumFile(byte[] readBytesFromFile){
         return getSHA1(readBytesFromFile);
     }
-    
 }
